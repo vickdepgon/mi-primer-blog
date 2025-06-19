9a3e8b39ff240ba23be5b27c00c671c6a67ac9b8
vickdepgon <vgonzalezdepetris@gmail.com>
2025-06-19 15:03:06 -0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from .models import Publicacion
 
 # Create your views here.
 def lista_public(request):
-    publicaciones=Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion') 
+    #publicaciones=Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion') 
+    publicaciones=Publicacion.objects.all() 
+
     return render(request, 'blog/lista_public.html', {'publicaciones':publicaciones})
 
